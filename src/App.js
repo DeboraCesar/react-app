c98d901ab68f8d56a908cf02e29f43663f91738d
DeboraCesar <deboravieira.cesar@gmail.com>
2023-07-13 18:18:47 +0100

    created app structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 
-import Search from "./Search";
-
-export default function App() {
+function App() {
   return (
-    <div className="Container">
-      <h1>Weather Search</h1>
-      <Search />
+    <div className="weather-app">
+      <div className="container">
+        <Weather defaultCity="London" />
+        <footer>
+          <p>
+            Coded by Diana M. {""}
+            <a
+              className="Git-link"
+              href="https://github.com/dianatech-id/react-weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open Source
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
+
+export default App;
